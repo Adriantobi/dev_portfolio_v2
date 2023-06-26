0e847eeda443dbec1d5e0b66472565329d4ec2e0
Adrian Tobi <talktotobi.a@gmail.com>
2023-06-26 11:41:23 +0100

    Remove Unused images and update CV

--- components/Footer.js
+++ components/Footer.js
@@ -24,7 +24,7 @@ export default function Footer() {
                       <li><FooterLink link='#about-me' name='About' /></li>
                       <li><FooterLink link='#projects' name='Projects' /></li>
                       <li><FooterLink link='#experience' name='Experience' /></li>
-                      <li><FooterLink link='/pdf/Adrian_Tobi_Resume_05-06-2023-11-13-52.pdf' name='Resume' /></li>
+                      <li><FooterLink link='/pdf/Adrian_Tobi_Resume_26-06-2023-11-33-09.pdf' name='Resume' /></li>
                   </ul>
               </div>
           </div>
