6373f1aff9b72c98a2bc3626308a2df8ff16907f
Adrian Tobi <talktotobi.a@gmail.com>
2023-07-13 08:45:20 +0100

    Font Fix 3??

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import '../css/globals.css'
 import { Inter } from 'next/font/google'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,10 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link href="https://api.fontshare.com/v2/css?f[]=array@400&display=swap" rel="stylesheet" />
-        <link href="https://api.fontshare.com/v2/css?f[]=clash-display@400&display=swap" rel="stylesheet" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
